4cbc3e2566867ae44b237a851d926e8e394aa2bb
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2022-03-01 13:30:29 +1300

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,73 @@
+<!-- #################### -->
+<!--         HTML         -->
+<!-- #################### -->
+
+<header>
+  <h1> Summer Gallery </h1>
+  <h2> My summer in photos </h2>
+</header>
+
+<main>
+  <h2> My Photos </h2>
+
+  <figure>
+    <img src="/placeholder.png" alt="A placeholder image.">
+    <figcaption> An image. </figcaption>
+  </figure>
+
+  <figure>
+    <img src="/placeholder.png" alt="A placeholder image.">
+    <figcaption> An image. </figcaption>
+  </figure>
+
+  <figure>
+    <img src="/placeholder.png" alt="A placeholder image.">
+    <figcaption> An image. </figcaption>
+  </figure>
+
+  <figure>
+    <img src="/placeholder.png" alt="A placeholder image.">
+    <figcaption> An image. </figcaption>
+  </figure>
+
+  <figure>
+    <img src="/placeholder.png" alt="A placeholder image.">
+    <figcaption> An image. </figcaption>
+  </figure>
+  
+</main>
+
+<!-- Footer -->
+
+<!-- ################### -->
+<!--         CSS         -->
+<!-- ################### -->
+
+<style>
+  header {
+    color: white;
+    background-color: black;
+    padding: 2em 5em;
+  }
+
+  header h1 {
+    font-size: xx-large;
+  }
+
+  header h2 {
+    font-size: large;
+  }
+
+  main {
+    padding: 1em 5em;
+  }
+
+  main h2 {
+    font-size: large;
+    font-weight: bold;
+  }
+
+  main figure img {
+    width: 100px;
+  }
+</style>
