848af604b44f56cf7349983ff14fdc95ad01f9f6
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2022-06-27 23:51:58 +0000

    Adds some example structure.

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from '$lib/Header.svelte'
+</script>
+
+<Header />
+
+<main>
+  <h2> SvelteKit </h2>
+
+  <p>
+    Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.
+  </p>
+</main>
